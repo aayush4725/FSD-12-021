d3d4a707c5f12f7e22756d5330103dd945b54d89
aayush4725 <aayush.25b01011332@abes.ac.in>
2026-07-30 11:58:42 +0530

    Dom1

---
+++ lab1/DOM.js
@@ -0,0 +1,7 @@
+//Dom document object model
+import{EventEmitter} from "events";
+const button = new EventEmitter();
+button.on("click",()=>{
+    console.log("Button Clicked");
+});
+button.emit("Click");
