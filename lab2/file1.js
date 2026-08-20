a663f1a596af77ae30474ad1109cb9ac882019e2
aayush4725 <aayush.25b01011332@abes.ac.in>
2026-08-20 11:02:02 +0530

    task commit

--- lab2/file1.js
+++ lab2/file1.js
@@ -1,2 +1,11 @@
-import { writeFile } from "fs/promises";
-await writeFile("hello.txt", "JS is easy");
+// import { writeFile,appendFile } from "fs/promises";
+import { writeFile, appendFile,readFile } from "fs/promises";
+
+// await writeFile("hello.txt","Hello JS");
+await appendFile("hello.txt","\n JS is much easy than othe r. ❤️❤️")
+// asynchronous type of file 
+await appendFile("hello.txt", "\naayush kumar ");
+const content=await readFile("hello.txt","utf-8")
+console.log(content);
+
+
